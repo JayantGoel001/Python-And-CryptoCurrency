3f17717a106e3982672127aed5fcf1a12041acf6
username <54479676+CormacKrum@users.noreply.github.com>
2020-08-27 06:27:36 +0530

    Initial Commit

--- Global API Endpoint.py
+++ Global API Endpoint.py
@@ -17,5 +17,20 @@ try:
 
     print(response)
     print(json.dumps(results, sort_keys=True, indent=4))
+
+    active_currency = results["data"]["active_cryptocurrencies"]
+    active_markets = results["data"]["active_market_pairs"]
+    bitcoin_percentage = results["data"]["btc_dominance"]
+    last_updates = results["data"]["last_updated"]
+    global_cap = results["data"]["quote"]["USD"]["total_market_cap"]
+    global_volume = results["data"]["quote"]["USD"]["total_volume_24h"]
+
+    print("Active Currency :", active_currency)
+    print("Active Market :", active_markets)
+    print("Bitcoin Percentage :", bitcoin_percentage)
+    print("Last Updated :", last_updates)
+    print("Global Capital :", global_cap)
+    print("Global volume :", global_volume)
+
 except (ConnectionError, Timeout, TooManyRedirects) as e:
     print(e)
