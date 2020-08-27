f7e31f0555cd8e88529e7d590fffaeb996d86810
username <54479676+CormacKrum@users.noreply.github.com>
2020-08-27 06:40:45 +0530

    Initial Commit

--- Global API Endpoint.py
+++ Global API Endpoint.py
@@ -1,6 +1,7 @@
 import json
 from requests import Session
 from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
+from datetime import datetime
 
 url = 'https://pro-api.coinmarketcap.com/v1/global-metrics/quotes/latest'
 headers = {
@@ -22,15 +23,24 @@ try:
     active_markets = results["data"]["active_market_pairs"]
     bitcoin_percentage = results["data"]["btc_dominance"]
     last_updates = results["data"]["last_updated"]
-    global_cap = results["data"]["quote"]["USD"]["total_market_cap"]
-    global_volume = results["data"]["quote"]["USD"]["total_volume_24h"]
+    global_cap = int(results["data"]["quote"]["USD"]["total_market_cap"])
+    global_volume = int(results["data"]["quote"]["USD"]["total_volume_24h"])
 
-    print("Active Currency :", active_currency)
-    print("Active Market :", active_markets)
+    active_currency_str = '{:,}'.format(active_currency)
+    active_markets_str = '{:,}'.format(active_markets)
+    global_cap_str = '{:,}'.format(global_cap)
+    global_volume_str = '{:,}'.format(global_volume)
+
+    # last_updates_date = datetime.fromtimestamp(last_updates).strftime('%B %d,%Y at %I:%M%p')
+
+    print("Active Currency :", active_currency_str)
+    print("Active Market :", active_markets_str)
     print("Bitcoin Percentage :", str(bitcoin_percentage), "%")
-    print("Last Updated :", last_updates)
-    print("Global Capital :", global_cap)
-    print("Global volume :", global_volume)
+
+    print("Global Capital :", global_cap_str)
+    print("Global volume :", global_volume_str)
+
+    print("The Information was last updated on :", last_updates)
 
 except (ConnectionError, Timeout, TooManyRedirects) as e:
     print(e)
