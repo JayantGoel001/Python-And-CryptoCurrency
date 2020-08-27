8472e6afb10ca4ba337f7253284b4234410bbd68
username <54479676+CormacKrum@users.noreply.github.com>
2020-08-27 08:21:22 +0530

    Initial Commit

---
+++ Listing API  Endpoint.py
@@ -0,0 +1,28 @@
+import json
+from requests import Session
+from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
+from datetime import datetime
+
+url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+
+headers = {
+    'Accepts': 'application/json',
+    'X-CMC_PRO_API_KEY': '46461c59-18e8-445d-93aa-0dfb99a9b0e0',
+}
+
+session = Session()
+session.headers.update(headers)
+
+response = session.get(url)
+results = response.json()
+
+print(json.dumps(results, sort_keys=True, indent=4))
+
+data = results["data"]
+
+for currency in data:
+    rank = currency["id"]
+    name = currency["name"]
+    symbol = currency["symbol"]
+
+    print(str(rank)+" : "+name+" ("+symbol+")")
