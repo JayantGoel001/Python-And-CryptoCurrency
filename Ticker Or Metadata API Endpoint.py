324587b6c4fdb751523de08f08dc0595115f3912
username <54479676+CormacKrum@users.noreply.github.com>
2020-08-27 11:06:38 +0530

    Initial Commit

--- Ticker Or Metadata API Endpoint.py
+++ Ticker Or Metadata API Endpoint.py
@@ -3,7 +3,7 @@ from requests import Session
 import numpy as np
 
 start = 1
-limit = 10
+limit = 5
 ar = list(np.arange(start, start + limit))
 ids = ",".join(str(x) for x in ar)
 
@@ -24,15 +24,15 @@ print(json.dumps(results, sort_keys=True, indent=4))
 
 data = results["data"]
 
-for currency in data:
-    rank = currency["rank"]
+for key, currency in data.items():
+    rank = currency["cmc_rank"]
     name = currency["name"]
     symbol = currency["symbol"]
 
     circulating_supply = int(currency["circulating_supply"])
     total_supply = int(currency["total_supply"])
 
-    quotes = currency["quotes"]["USD"]
+    quotes = currency["quote"]["USD"]
     market_cap = quotes["market_cap"]
     hour_change = quotes["percent_change_1h"]
     day_change = quotes["percent_change_24h"]
@@ -40,3 +40,21 @@ for currency in data:
 
     price = quotes["price"]
     volume = quotes["volume_24h"]
+
+    volume_string = "{:,}".format(volume)
+    market_cap_string = "{:,}".format(market_cap)
+    circulating_supply_string = "{:,}".format(circulating_supply)
+    total_supply_string = "{:,}".format(total_supply)
+
+    print(str(rank) + " : " + name + "( " + symbol + " ) ")
+    print('Market Cap: $' + market_cap_string)
+    print('Price: $' + str(price))
+    print('24h Volume : $' + volume_string)
+    print('Hour Change : ' + str(hour_change) + '%')
+    print('Day Change : ' + str(day_change) + '%')
+    print('Week Change : ' + str(week_change) + '%')
+
+    print("Total Supply : " + total_supply_string)
+    print("Circulating Supply : " + total_supply_string)
+    print("Percentage of coins in circulation : " + str(int(circulating_supply / total_supply)) + "%")
+    print()
